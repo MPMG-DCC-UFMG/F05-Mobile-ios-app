33491159d80e4abb37d984098cb3c2edbb6a676d
Walter José <walterjgsp@gmail.com>
2020-09-10 10:46:43 -0300

    Using distance on list

--- mpapp/mpapp/ui/components/items/PublicWorkListItem.swift
+++ mpapp/mpapp/ui/components/items/PublicWorkListItem.swift
@@ -1,14 +1,18 @@
 import SwiftUI
+import Resolver
+import CoreLocation
 
 struct PublicWorkListItem: View {
     var publicWork: PublicWork
+    
+    @ObservedObject private var locationManager: LocationManager = Resolver.resolve()
     
     var body: some View {
         VStack{
             HStack{
                 Text("\(publicWork.name)").h2()
                 Spacer()
-                Text("200m").h3()
+                Text(getDistance(location: locationManager.location)).h3()
             }
             HStack{
                 Text("\(publicWork.formatedAddress())").h4()
@@ -16,9 +20,25 @@ struct PublicWorkListItem: View {
             }.padding(.top,10)
             
         }.padding()
-        .frame(minWidth: 0, maxWidth: .infinity)
-        .background(ColorProvider.grey3)
-        .cornerRadius(8)
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .background(ColorProvider.grey3)
+            .cornerRadius(8)
+    }
+    
+    private func getDistance(location: CLLocation?) -> String{
+        if(publicWork.address == nil || location == nil) {
+            return "--"
+        }
+        let currLocation = CLLocation(latitude: publicWork.address!.latitude, longitude: publicWork.address!.longitude)
+        return formatDistance(distance: location!.distance(from: currLocation))
+    }
+    
+    private func formatDistance(distance: Double)->String{
+        if(distance>1000){
+            return String(format: "%.0f km", distance/1000)
+        }else{
+            return String(format: "%.0f m", distance)
+        }
     }
 }
 
